500d7a5b8de843fe5285bad9298a6f49dc8af93d
Eugene Kolenko <kolenko.evg@gmail.com>
2022-08-30 20:05:54 +0600

    Initial commit

---
+++ bot/main.py
@@ -0,0 +1,16 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+bot = Bot(token='5407304437:AAHiAugKKNrhHTtws17-OewjdfgRyepTSCk')
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def answer_start_command(message: types.Message):
+    await message.answer(text='Привет')
+
+@dp.message_handler(text=['привет'])
+async def answer_prvt_text(message: types.Message):
+    await message.answer(text='+')
+
+executor.start_polling(dp)
